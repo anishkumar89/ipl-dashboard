b37af718ffcc8feef95b5b0e104977cf717f6fdd
Anish Kumar <anish.hercules@gmail.com>
2021-04-26 16:09:01 +1000

    IPL Dashboard Spring Boot API

---
+++ src/main/java/io/javatech/ipldashboard/controller/TeamController.java
@@ -0,0 +1,47 @@
+package io.javatech.ipldashboard.controller;
+
+import java.util.List;
+import java.time.LocalDate;
+import io.javatech.ipldashboard.model.Team;
+import io.javatech.ipldashboard.model.Match;
+import org.springframework.web.bind.annotation.*;
+import io.javatech.ipldashboard.repository.TeamRepository;
+import io.javatech.ipldashboard.repository.MatchRepository;
+
+@RestController
+@CrossOrigin
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+
+    @GetMapping("/team")
+    public Iterable<Team> getAllTeam() {
+        return this.teamRepository.findAll();
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName,4));
+        return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+                teamName,
+                startDate,
+                endDate
+        );
+    }
+
+}
